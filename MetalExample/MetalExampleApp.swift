328b2dcc0303661d8fcbf9bfdd6cf1542b34a63c
Patrick Abadi <patrickabadi@gmail.com>
2020-12-23 11:55:36 -0800

    Initial Commit

---
+++ MetalExample/MetalExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalExampleApp.swift
+//  MetalExample
+//
+//  Created by Patrick Abadi on 2020-12-23.
+//
+
+import SwiftUI
+
+@main
+struct MetalExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
